a3406e0d1837b83fe931d11d55110bbd59dddc8d
gabb096 <gab96.leva@gmail.com>
2022-06-06 22:56:00 +0200

    User can modify number of furniture elements

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 let DOM_MinusButton = document.querySelector(".minus");
 let DOM_PlusButton = document.querySelector(".plus");
 
+
 DOM_PlusButton.addEventListener("click", function() 
 {  
-    let n = document.querySelectorAll(".furniture").length;  
+    let DOM_FornitureContainer = document.querySelector(".furnitureContainer");
+    let DOM_Forniture = document.querySelectorAll(".furniture");
 
-    if( n < 8)
-    {
-        let DOM_c = document.querySelector(".furnitureContainer");
-        DOM_c.innerHTML += ` 
-        <div class="furniture">
+    let n = DOM_Forniture.length;
+    if(n<8)
+    {    let elem = document.createElement('div');
+        elem.innerHTML = `
             <label class="label"> Arredamento ${n+1} </label>
             <input list="List2"  class="input" placeholder="Arredamento" >
-            <input type="number" class="input" min="0" placeholder="Superficie in Cm^2"> 
-        </div>`;
+            <input type="number" class="input" min="0" placeholder="Superficie Cm^2"> `;
+        elem.className = 'furniture';
+        DOM_FornitureContainer.appendChild(elem);
     }
-    console.log(n);
 });
 
 DOM_MinusButton.addEventListener("click", function() 
 {  
-    const n = document.querySelectorAll(".furniture").length;  
+    let DOM_FornitureContainer = document.querySelector(".furnitureContainer");
+    let DOM_Forniture = document.querySelectorAll(".furniture");
 
+    let n = DOM_Forniture.length;
     if( n > 0)
     {
-
+        DOM_FornitureContainer.removeChild(DOM_Forniture[n-1]);
     }
 });
